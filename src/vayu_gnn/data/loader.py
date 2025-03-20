8a5d5f13e0828fa0413375b790b996f7cd084bfc
4Freye <4freye@gmail.com>
2025-03-20 11:56:00 -0700

    added merging of settlements data

--- src/vayu_gnn/data/loader.py
+++ src/vayu_gnn/data/loader.py
@@ -56,9 +56,13 @@ class DataLoader:
                 f"GHS_{building_type}_building_{dimension}_b{size}.parquet"
             )
     
-    # def settlements(self, city):
-    #     files_to_load = self.dbx_helper.list_files_in_folder(folder_path = f'{self.dbx_helper.clean_input_path}/settlement/{city}' )
-    #     return [self.dbx_helper.read_parquet(file) for file in files_to_load]
+    def settlements(self, city):
+        files_to_load = self.dbx_helper.list_files_in_folder(folder_path = f'{self.dbx_helper.clean_input_path}/settlement/{city}' )
+        df = self.dbx_helper.read_parquet(self.dbx_helper.clean_input_path, f'settlement/{city}', files_to_load[0])
+        for file in files_to_load[1:]:
+            _df = self.dbx_helper.read_parquet(self.dbx_helper.clean_input_path, f'settlement/{city}', file)
+            df = df.merge(_df, how='left', on='node_id')
+        return df
 
     def multi_load(self, city:str, sources:list):
         """Loads multiple sourches at once.
@@ -79,7 +83,10 @@ class DataLoader:
     def multi_merge(self, remove_dfs = False):
         df = self.dfs['sensor_data']
         for source in self.dfs.keys() - {"sensor_data"}:
-            df = df.merge(how = 'left', on=['node_id','date','hour'])
+            if ['node_id','date','hour'] in self.dfs[source]:
+                df = df.merge(self.dfs[source], how = 'left', on=['node_id','date','hour'])
+            else:
+                df = df.merge(self.dfs[source], how = 'left', on=['node_id'])
         if remove_dfs:
             del self.dfs
         return df
